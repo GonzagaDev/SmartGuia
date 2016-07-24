61927247e8bccd7f1efa77b5f4131100484e4b6d
cristiano.gonzaga <cristiano.gonzaga@gmail.com>
2016-07-24 13:48:09 -0300

    Criação da classe de conexão com o Banco

--- SmartGuia/app/src/main/java/auditsolution/com/br/smartguia/dataBase/CriaBanco.java
+++ SmartGuia/app/src/main/java/auditsolution/com/br/smartguia/dataBase/CriaBanco.java
@@ -1,6 +1,8 @@
 package auditsolution.com.br.smartguia.dataBase;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -36,5 +38,48 @@ public class CriaBanco extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS" + TABELA);
         onCreate(db);
     }
+
+    public class BancoController {
+
+        private SQLiteDatabase db;
+        private CriaBanco banco;
+
+        public BancoController(Context context) {
+            banco = new CriaBanco(context);
+        }
+
+        public String insereDado(String titulo, String autor, String editora) {
+            ContentValues valores;
+            long resultado;
+
+            db = banco.getWritableDatabase();
+            valores = new ContentValues();
+            valores.put(CriaBanco.MAC, titulo);
+            valores.put(CriaBanco.NREFERENCIA, autor);
+            valores.put(CriaBanco.ENDERECO, editora);
+
+            resultado = db.insert(CriaBanco.TABELA, null, valores);
+            db.close();
+
+            if (resultado == -1)
+                return "Erro ao inserir registro";
+            else
+                return "Registro Inserido com sucesso";
+
+        }
+        public Cursor carregaDados(){
+            Cursor cursor;
+            String[] campos =  {banco.ID,banco.NREFERENCIA};
+            db = banco.getReadableDatabase();
+            cursor = db.query(banco.TABELA, campos, null, null, null, null, null, null);
+
+            if(cursor!=null){
+                cursor.moveToFirst();
+            }
+            db.close();
+            return cursor;
+        }
+    }
+
 }
 
